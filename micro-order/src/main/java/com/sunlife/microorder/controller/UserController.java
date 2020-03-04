76e16e18e6d0aef01080a7e7c851b685ba0b492f
sunlife <1298629573@qq.com>
2020-03-04 23:19:43 +0800

    创建

--- micro-order/src/main/java/com/sunlife/microorder/controller/UserController.java
+++ micro-order/src/main/java/com/sunlife/microorder/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.sunlife.microorder.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UserController {
 
+    public static Boolean CAN_RESULT = true;
+
     @RequestMapping("/queryUser")
     public String queryUser() {
-        log.info("micro-order====queryUser");
-        return "micro-order====queryUser";
+        if (CAN_RESULT) {
+            log.info("micro-order====queryUser");
+            return "micro-order====queryUser";
+        } else {
+            int a = 1/0;
+            return "micro-order====queryUser====failed";
+        }
+    }
+
+    @RequestMapping("/canResult/{canResult}")
+    public String setCanResult(@PathVariable Boolean canResult) {
+        CAN_RESULT = canResult;
+        return CAN_RESULT.toString();
     }
 }
--- micro-web/src/main/java/com/sunlife/microweb/controller/StudentController.java
+++ micro-web/src/main/java/com/sunlife/microweb/controller/StudentController.java
@@ -1,4 +1,19 @@
 package com.sunlife.microweb.controller;
 
+import com.sunlife.microweb.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @RequestMapping("/query")
+    public String query(Integer id) {
+        return studentService.query(id);
+    }
+
 }
