69111f985b69a4ff704e508b41ffde8941805567
sunlife <1298629573@qq.com>
2020-03-09 23:02:26 +0800

    测试mysql多线程批量插入死锁

--- micro-order/src/main/java/com/sunlife/microorder/service/impl/StucentServiceImpl.java
+++ micro-order/src/main/java/com/sunlife/microorder/service/impl/StucentServiceImpl.java
@@ -2,11 +2,24 @@ package com.sunlife.microorder.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.sunlife.microorder.bean.Student;
+import com.sunlife.microorder.dao.StudentDao;
 import com.sunlife.microorder.service.StudentService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 @Service
 public class StucentServiceImpl implements StudentService {
+
+    private Integer count = 4;
+
+    private CountDownLatch cdl = new CountDownLatch(count);
+
+    private StudentDao studentDao;
+
     @Override
     public String errorMsg(Integer id) {
         try {
@@ -19,5 +32,31 @@ public class StucentServiceImpl implements StudentService {
 //            return e.getMessage();
             throw e;
         }
+    }
+
+    @Override
+    public int testBatchInsert() {
+        for (int i = 0; i < count; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        cdl.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    List<Student> lists = new ArrayList<>();
+                    for (int i = 0; i < 500; i++) {
+                        lists.add(new Student(Thread.currentThread().getName()));
+                    }
+                    for (Student student: lists) {
+                        System.out.println(student.getName());
+                    }
+                    System.out.println(studentDao.insertBatch(lists));
+                }
+            }).start();
+            cdl.countDown();
+        }
+        return 0;
     }
 }
